9e44c0815553c6d555f8607d45cfeee3050b0c50
amruta2503 <142072684+amruta2503@users.noreply.github.com>
2026-01-10 09:32:27 +0530

    Update check_palindrome.py

--- check_palindrome.py
+++ check_palindrome.py
@@ -1,7 +1,7 @@
 def is_Palindrome_brute(string):
     rev_string = ""
+    string = string.lower()
     n = len(string)
-    
     for i in range(n-1,-1,-1):
         rev_string += string[i]
         
@@ -9,11 +9,28 @@ def is_Palindrome_brute(string):
         print("using brute:",True)
     else:
         print("using brute:",False)
+
+def is_Palindrome_better(string):
+    string = string.lower()
+    left = 0
+    right = len(string) - 1
     
+    while left < right:
+        if string[left] != string[right]:
+            return False
+        
+        left+=1
+        right-=1
+        
+    return True
     
 def is_Palindrome_optimal(string):
+    string = string.lower()
     return string == string[::-1]
 
+
+ 
 string = input("Enter the string:")
 is_Palindrome_brute(string)
+print("using better:",is_Palindrome_better(string))
 print("using optimal:",is_Palindrome_optimal(string))
